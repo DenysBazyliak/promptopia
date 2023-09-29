29a8b6d06c64e85da7a4df12fdc75e8a222e17b6
Denys Bazyliak <franciskdelyamon@gmail.com>
2023-09-29 20:28:02 +0200

    feat: Add working home page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import '@styles/globals.css';
-
+import Nav from 'components/Nav';
+import Provider from 'components/Provider';
 export const metadata= {
     title:'Promptopia',
     description:'Discover...'
@@ -9,12 +10,15 @@ const RootLayout = ({children}) => {
   return(
       <html lang={'en'}>
       <body>
+      <Provider>
       <div className={'main'}>
           <div className={'gradient'}/>
       </div>
       <main className={'app'}>
+          <Nav/>
           {children}
       </main>
+      </Provider>
       </body>
       </html>
 
--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,8 @@
+import Feed from 'components/Feed'
+
 const Home = () => {
   return(
+      <>
       <section className={'w-full flex-center flex-col'}>
             <h1 className={'head_text text-center'}>
                 Discover and share
@@ -9,8 +12,9 @@ const Home = () => {
           <p className={'desc text-center'}>
               Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts
           </p>
+          <Feed/>
       </section>
-
+      </>
   )
 }
 export default Home
