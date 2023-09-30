cc327613b090d5038cbd58f64219504b476ac2cc
Denys Bazyliak <franciskdelyamon@gmail.com>
2023-09-30 14:36:32 +0200

    feat: Implement new Prompt schema and api call

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,4 +1,30 @@
+'use client'
+
+import {useState} from "react";
+import {useSession} from "next-auth/react";
+import {useRouter} from "next/navigation";
+import Form from '../../components/Form'
+
 const CreatePrompt = () => {
-    return <div>prompt</div>
+    const [submitting, setSubmitting] = useState(false)
+    const [post, setPost] = useState({
+        prompt: '',
+        tag: ''
+    })
+    const createPrompt = async (e) => {
+
+    }
+    return (
+        <Form
+            type={'Create'}
+            post={post}
+            submitting={submitting}
+            setPost={setPost}
+            setSubmitting={setSubmitting}
+            handleSubmit={createPrompt}
+        />
+    )
+
+
 }
 export default CreatePrompt
--- utils/database.js
+++ utils/database.js
@@ -4,6 +4,7 @@ let isConnected = false// track the connection
 
 export const connectToDB = async ()=>{
     mongoose.set('strictQuery',true )
+
     if(isConnected){
         console.log('MongoDB is already connected ')
         return
