adcdb3d2d8d1fd599ab2af58c7951b329b3c8bb8
Denys Bazyliak <franciskdelyamon@gmail.com>
2023-10-03 19:29:15 +0200

    feat: create Profile page and request to get them

---
+++ app/profile/page.js
@@ -0,0 +1,41 @@
+'use client'
+
+import { useState, useEffect } from 'react' ;
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation' ;
+import Profile from '../../components/Profile'
+
+const MyProfile = () => {
+    const [myPostArray,setMyPostArray]=useState([])
+
+    const {data:session}= useSession()
+    const router = useRouter();
+
+    const handleEdit = (post)=>{
+        router.push(`/update-prompt?id=${post._id}`)
+    }
+    const handleDelete = async ()=>{
+
+    }
+
+    useEffect(()=>{
+        const fetchPosts = async ()=>{
+            const response = await fetch(`/api/users/${session?.user.id}/posts`);
+            const data = await response.json()
+
+            setMyPostArray(data.flat())
+        }
+        if (session?.user.id) fetchPosts()
+    },[])
+    return (
+        <Profile
+        name={'my'}
+        desc={'Welcome to your personalized profile page UwU'}
+        data={myPostArray}
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
+        />
+    )
+}
+
+export default MyProfile
